60214fe2f45f90a4b45ffa05cd6ecfe68ef9125b
Shubham Yelekar <70461738+Shubham-yelekar@users.noreply.github.com>
2024-05-26 18:46:35 +0530

    init

---
+++ to-do-list/app.js
@@ -0,0 +1,24 @@
+
+    const addBtn = document.getElementById("add-btn");
+    const todoInput = document.getElementById("todo-input");
+    const todoList = document.getElementById("todo-list");
+  
+    // Function to add a new task
+
+    // Create delete button with Font Awesome icon
+       
+  
+    // Append delete button to the list item
+    
+    // Append list item to the todo list
+  
+  
+    // Clear the input field
+   
+  
+    // Add event listener to the delete button
+    
+    // Add task when clicking the "Add Task" button
+   
+  
+    // Add task when pressing the "Enter" key
