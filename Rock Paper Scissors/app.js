6e20323d9c4f1aa562716d2d1a98226c12dcaf3b
Shubham Yelekar <70461738+Shubham-yelekar@users.noreply.github.com>
2024-10-12 20:18:16 +0530

    rock paper scissors complete

--- Rock Paper Scissors/app.js
+++ Rock Paper Scissors/app.js
@@ -1,17 +1,79 @@
-console.log("Rock Paper Scissors?")
+const buttons = document.querySelectorAll('.secondary-btn');
+const compScoreDisplay = document.getElementById('com-score');
+const userScoreDisplay = document.getElementById('user-score');
+const rockEmoji = '🤟';
+const scissorsEmoji = '✂️';
+const paperEmoji ='📰';
+
+let userScore = 0; // Initialize user's score
+let computerScore = 0; // Initialize computer's score
+
+function updateScore(verdict) {
+  if (verdict === 'User Wins') {
+    userScore++;
+  } else if ( verdict === 'Computer Wins' ){
+    computerScore++; 
+  } 
+  userScoreDisplay.textContent = userScore; 
+  compScoreDisplay.textContent = computerScore; 
+}
 
 function getComputerChoice(){
+
 let ans
- let randomNumber = Math.floor((Math.random()*10)/3 + 1)
-  if (randomNumber == '1'){
+let ansEmoji
+ let randomNumber = Math.floor(Math.random()*3)
+  if (randomNumber == '0'){
     ans = 'rock'
-  }else if (randomNumber == '2'){
+    ansEmoji = rockEmoji
+  }else if (randomNumber == '1'){
     ans = 'paper'
+    ansEmoji = paperEmoji
   }else {
     ans = 'scissors'
+    ansEmoji = scissorsEmoji
   }
- return console.log(ans)
+  console.log("Comp" , ans)
+  document.getElementById('computer-choice').textContent = ansEmoji;
+  return ans;
 
 }
-console.log(getHumanChoice())
-console.log(getComputerChoice())
+
+buttons.forEach((button) =>{
+  button.addEventListener('click', HandleChoice)
+})
+
+function HandleChoice(e) {
+  const computerChoice = getComputerChoice();
+  const userChoice = e.target.value; 
+ 
+    // Update user choice display with the corresponding emoji
+    if (userChoice === 'rock') {
+      document.getElementById('user-choice').textContent = rockEmoji; // Use the rock emoji
+  } else if (userChoice === 'paper') {
+      document.getElementById('user-choice').textContent = paperEmoji; // Use the paper emoji
+  } else if (userChoice === 'scissors') {
+      document.getElementById('user-choice').textContent = scissorsEmoji; // Use the scissors emoji
+  }
+  if(computerChoice === userChoice) {
+      console.log("Tie")
+      return updateScore("Tie")
+  } else if (computerChoice !== userChoice) {
+    if ((computerChoice === 'rock' && userChoice === 'scissors') ||
+      (computerChoice === 'scissors' && userChoice === 'paper') ||
+      (computerChoice === 'paper' && userChoice === 'rock') 
+    ){
+      console.log("Computer Wins")
+      return updateScore("Computer Wins")
+
+    } else {
+
+      console.log("User Wins")
+      return updateScore("User Wins")
+    }
+}
+  
+}
+
+
+
